71404b451c5837592897285e4e6680ea05bd42e0
fnzyyy8 <farhanshidayat@gmail.com>
2023-12-03 02:07:23 +0700

    update: Our client database using firestore database

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,6 @@
-/* eslint-disable no-unused-vars */
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,3 +16,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const dbClients = getFirestore(app);
